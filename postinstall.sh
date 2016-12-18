aad857c9f582bf553fefaaf629435a8adf7416de
eru <epenkman@thoughtworks.com>
2016-12-18 21:07:05 +1000

    handle ipfs folder existing

---
+++ postinstall.sh
@@ -0,0 +1,1 @@
+CXXFLAGS=-I/usr/local/opt/openssl/include LDFLAGS=-L/usr/local/opt/openssl/lib ./node_modules/.bin/electron-rebuild
--- renderer.js
+++ renderer.js
@@ -3,19 +3,23 @@
 // All of the Node.js APIs are available in this process.
 const $ = require('jquery')
 const {dialog} = require('electron').remote
-var fs = require('fs');
+var fs = require('fs')
+var ipfsWrapper = require('./lib/ipfs-wrapper.js')
+
+ipfsWrapper.init().then(() => {
+  console.log(ipfsWrapper.get)
+})
 
 $('#the-file-to-upload').on('click', () => {
   const filePath = dialog.showOpenDialog({properties: ['openFile']})
-  console.log(filePath);
-  if(typeof filePath != 'object' || !filePath[0]){
-    console.log('Cannot read file: ', filePath);
-    return;
+  console.log(filePath)
+  if (typeof filePath !== 'object' || !filePath[0]) {
+    console.log('Cannot read file: ', filePath)
+
+    return
   }
   fs.readFile(filePath[0], function (err, data) {
-    if (err) return console.log(err);
+    if (err) return console.log(err)
     console.log('file: ', data.length)
-
-      //do ipfs thing.
-  });
-});
+  })
+})
