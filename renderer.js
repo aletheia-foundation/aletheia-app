b064a488c5dfb835f5be7343b3009703263209ab
eru <epenkman@thoughtworks.com>
2016-12-18 22:32:32 +1000

    display notification when file has been added successfully

--- renderer.js
+++ renderer.js
@@ -3,23 +3,57 @@
 // All of the Node.js APIs are available in this process.
 const $ = require('jquery')
 const {dialog} = require('electron').remote
-var fs = require('fs')
-var ipfsWrapper = require('./lib/ipfs-wrapper.js')
+const fs = require('fs')
+const ipfsWrapper = require('./lib/ipfs-wrapper.js')
+
+$('#upload-success-div').hide()
 
 ipfsWrapper.init().then(() => {
-  console.log(ipfsWrapper.get)
+  // ipfsWrapper.get('')
+}).catch((err) => {
+  console.error('IPFS error:', err)
 })
 
 $('#the-file-to-upload').on('click', () => {
   const filePath = dialog.showOpenDialog({properties: ['openFile']})
-  console.log(filePath)
   if (typeof filePath !== 'object' || !filePath[0]) {
     console.log('Cannot read file: ', filePath)
-
     return
   }
-  fs.readFile(filePath[0], function (err, data) {
-    if (err) return console.log(err)
-    console.log('file: ', data.length)
+  uploadFileToIpfs(filePath[0])
+  .then((result) => {
+    console.log('result')
+    $('#upload-success-div').show()
+    $('#upload-success-file-name').text(result.path)
+    $('#upload-success-file-id').text(result.hash)
+  })
+  .catch((err) => {
+    console.error(err)
+    $('#error-div').show()
+    $('#error-div').text(`Unable to add file +${filePath}`)
   })
 })
+
+function uploadFileToIpfs (filePath) {
+  return new Promise((res, rej) => {
+    fs.readFile(filePath, function (err, data) {
+      if (err) {
+        throw {
+          err: err
+        }
+      }
+      console.log('file: ', data.length)
+      ipfsWrapper.addFile({
+        name: 'blah.jpg',
+        fileData: data
+      }).then((result) => {
+        res(result)
+      }).catch((err) => {
+        console.error(err.stack)
+        throw {
+          err: err
+        }
+      })
+    })
+  })
+}
