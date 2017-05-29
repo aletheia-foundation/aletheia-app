a8a8c46926a4d709abfad24bc7bf327e3b70e85f
roo2 <roo2@protonmail.com>
2017-05-29 19:28:27 +1000

    replace console with --mine

--- scripts/start-geth-testnet.sh
+++ scripts/start-geth-testnet.sh
@@ -10,5 +10,5 @@ if [ ! -z "$TESTNET_ACCOUNTS" ]; then
   geth --datadir $TEST_DATA_DIR --rpc --rpcapi eth,net,web3,personal \
       --networkid 123 --nodiscover  --password ./config/test/password \
       --unlock $TESTNET_ACCOUNTS \
-      console
+      --mine 
 fi
