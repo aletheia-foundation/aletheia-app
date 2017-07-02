1a546536115acb14eeae8df5ed6ade1697d7b900
roo2 <roo2@protonmail.com>
2017-07-02 16:51:54 +1000

    add contract build output to allow testnet nodes to use the same contract addresses

--- truffle.js
+++ truffle.js
@@ -1,14 +1,14 @@
 module.exports = {
   networks: {
+    testnet: {
+        host: "localhost",
+        port: 8545,
+        network_id: "123039281"
+    },
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
-    },
-    testnet: {
-        host: "localhost",
-        port: 8545,
-        network_id: "123"
     }
   }
 };
