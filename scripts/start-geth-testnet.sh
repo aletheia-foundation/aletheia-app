7de7144b8c97413a668c174c1a0ac11ff9d2c26c
roo2 <roo2@protonmail.com>
2017-08-03 20:45:51 +1000

    update bootnode

--- scripts/start-geth-testnet.sh
+++ scripts/start-geth-testnet.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 source ./scripts/env-testnet.sh
 
+BOOTNODE_1_IP=`dig +short theserverbythe.stream | awk '{print $1}'`
+
+
 # Even if we are not the admin(rich) node,
 # still init the network with the same genesis.testnet.json to make networks compatible
 if [ ! -d  $TEST_DATA_DIR ]; then
@@ -22,6 +25,6 @@ TESTNET_ACCOUNTS=`./scripts/get-testnet-addresses.sh`
 
 geth --datadir $TEST_DATA_DIR --rpc --rpcapi eth,net,web3,personal \
   --networkid 123039281 --password $TEST_NET_PASSWORD \
-  --unlock $TESTNET_ACCOUNTS \
-  --bootnodes enode://62ab83384fe32644223de24565a7e6712304eadabd8a378ba00015499dd0bf189b374d13c9fdcd9cf1c8e750ffda631e16be5a6462ebefbe5b09c40baa505d80@127.0.0.1:30303 \
+  --unlock "$TESTNET_ACCOUNTS" \
+  --bootnodes enode://68df974606fb67032ff513e7d54733d14f49783bd63c9469eb21c1c5422e898d581e880cbf0944a22ffea740f1d0ef70a3449a898bd6a81a8466de6b61146947@$BOOTNODE_1_IP:30303 \
   js ./scripts/mine.js
