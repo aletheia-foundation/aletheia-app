71de90e0389b54b1c09c2ba42fbee38c9fbb38e3
roo2 <roo2@protonmail.com>
2017-08-30 20:13:49 +1000

    refactor submit paper page

--- app/submit-paper/submit-paper.js
+++ app/submit-paper/submit-paper.js
@@ -1,7 +1,7 @@
 const dialog = require('electron').remote.dialog
 const config = require('config')
 
-const submitPaperView = require('./submit-paper-view')
+const view = require('./submit-paper-view')
 const IpfsClientFactory = require('../common/ipfs-client/ipfs-client-factory')
 const Web3ClientFactory = require('../common/web3/web3-client-factory')
 
@@ -10,34 +10,73 @@ const web3ClientPromise = Web3ClientFactory.getDefaultInstance()
 const ipfsClient = IpfsClientFactory.getDefaultInstance()
 
 web3ClientPromise.then((web3Client) => {
-  web3Client.on('peer-update', (err, numPeers) => {
-    if (err) {
-      submitPaperView.showEthereumError(`Error conecting to alethia blockchian node at: ${config.get('web3.url')}`)
-    } else if (numPeers === 0) {
-      submitPaperView.showEthereumError('No alethia blockchain peers found')
-    } else if (numPeers > 0) {
-      submitPaperView.setEthereumPeers(numPeers)
-    }
+  new SubmitPaperController({
+    web3Client,
+    ipfsClient,
+    view
   })
-  web3Client.on('balance-update', (err, balance) => {
-    if (err || !balance) {
-      submitPaperView.showEthereumBalance(`Error getting balance`)
-    } else {
-      submitPaperView.showEthereumBalance({balance})
-    }
-  })
-  web3Client.createAccountIfNotExist().then((accountHash) => {
-    submitPaperView.showEthereumAccount({accountHash})
-  }).catch((e) => {
-    console.error(e, e.stack)
-    submitPaperView.showEthereumAccount('')
-  })
+}).catch((err) => {
+  console.error(err, err.stack)
+  view.showEthereumError('Error initialising blockchain')
+})
 
-  submitPaperView.on('clickSelectFile', () => {
+class SubmitPaperController {
+  constructor ({web3Client, ipfsClient, view}) {
+    this._web3Client = web3Client
+    this._ipfsClient = ipfsClient
+    this._view = view
+
+    this._web3Client.on('peer-update', this._onWeb3PeerUpdate.bind(this))
+    this._web3Client.on('balance-update', this._onBalanceUpdate.bind(this))
+    this._ipfsClient.on('peer-update', this._onIpfsPeerUpdate.bind(this))
+
+    this._view.on('clickSelectFile', this._onSelectFileClick.bind(this))
+
+    this._web3Client.createAccountIfNotExist().then((accountHash) => {
+      this._view.showEthereumAccount({accountHash})
+    }).catch((e) => {
+      console.error(e, e.stack)
+      this._view.showEthereumAccount('')
+    })
+  }
+
+  _onWeb3PeerUpdate (err, numPeers) {
+    if (err) {
+      this._view.showEthereumError(`Error conecting to alethia blockchian node at: ${config.get('web3.url')}`)
+    } else if (numPeers === 0) {
+      this._view.showEthereumError('No alethia blockchain peers found')
+    } else if (numPeers > 0) {
+      this._view.setEthereumPeers(numPeers)
+    }
+  }
+
+  _onBalanceUpdate (err, balance) {
+    if (err || !balance) {
+      this._view.showEthereumBalance(`Error getting balance`)
+    } else {
+      this._view.showEthereumBalance({balance})
+    }
+  }
+
+  _onIpfsPeerUpdate (err, numPeers) {
+    if (err) {
+      view.showError(`Error conecting to Aletheia filesystem gateway at  ${config.get('ipfs.gatewayUrl')}`)
+    } else if (numPeers === 0) {
+      view.showError('No alethia filesystem peers found')
+    } else if (numPeers > 0) {
+      view.setPeers(numPeers)
+    }
+  }
+
+  _onSelectFileClick () {
+    if (!hasEnoughBalance()) {
+      this.promptTopUpBalance()
+    }
+
     const filePath = dialog.showOpenDialog({properties: ['openFile']})
 
     if (typeof filePath !== 'object' || !filePath[0]) {
-      submitPaperView.showError(`Cannot read file: ${filePath}`)
+      this._view.showError(`Cannot read file: ${filePath}`)
       return
     }
     const fileName = filePath[0].match(/[^/]+$/)
@@ -51,35 +90,22 @@ web3ClientPromise.then((web3Client) => {
         throw {err: 'result[0].hash was null', result }
       }
 
-      return web3Client.indexNewFile(result[0].hash).then((transactionHash) => {
-        submitPaperView.showUploadInProgress({transactionHash})
-        return web3Client.awaitIndexNewFile(transactionHash)
+      return this._web3Client.indexNewFile(result[0].hash).then((transactionHash) => {
+        this._view.showUploadInProgress({transactionHash})
+        return this._web3Client.awaitIndexNewFile(transactionHash)
       }).then((blockHash) => {
           // will probably have to poll for inclusion in the blockchain =(
         if (typeof blockHash !== 'string') {
           throw { err: 'hash of ethereum block was invalid', blockHash }
         }
-        submitPaperView.showUploadSuccess({
+        this._view.showUploadSucces({
           hash: result[0].hash,
           path: result[0].path
         })
       })
     }).catch((err) => {
       console.error(err, err.stack)
-      submitPaperView.showUploadError({path: fileName})
+      this._view.showUploadError({path: fileName})
     })
-  })
-}).catch((err) => {
-  console.error(err, err.stack)
-  submitPaperView.showEthereumError('Error initialising blockchain')
-})
-
-ipfsClient.on('peer-update', (err, numPeers) => {
-  if (err) {
-    submitPaperView.showError(`Error conecting to Aletheia filesystem gateway at  ${config.get('ipfs.gatewayUrl')}`)
-  } else if (numPeers === 0) {
-    submitPaperView.showError('No alethia filesystem peers found')
-  } else if (numPeers > 0) {
-    submitPaperView.setPeers(numPeers)
   }
-})
+}
