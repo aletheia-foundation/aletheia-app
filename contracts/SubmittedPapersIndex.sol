45965ea9f7f3b1defa4e13320830efbc39868b46
Pushkal Katara <katarapushkal@gmail.com>
2017-11-26 07:28:15 +0530

    solves #53

--- contracts/SubmittedPapersIndex.sol
+++ contracts/SubmittedPapersIndex.sol
@@ -1,14 +1,54 @@
 pragma solidity ^0.4.7;
 
+import "./FIFO.sol";
+import "./LinkedList.sol";
+
 contract SubmittedPapersIndex {
-  bytes32[] public store;
 
-  function push(bytes32 _hash) {
+  FIFO public ManuscriptQueue;
+  DoublyLinkedList.data public ReviewedManuscripts;
+
+  function FifoClient(){
+    ManuscriptQueue = new FIFO();
+  }
+  /**
+    Queue(FIFO) Accept Manuscripts and add to queue to be vetted.
+  */
+  function push(bytes32 _hash)
+    public
+    returns(uint ManuscriptNumber)
+  {
     require(_hash != 0x00);
-    store.push(_hash);
+    uint ManuscriptNum = ManuscriptQueue.push(_hash);
+    return ManuscriptNum;
   }
-
-  function getAll() constant returns (bytes32[] hashes) {
-    return store;
+  /**
+    Function called after a Manuscript is vetted
+    Append vetted manuscript to ReviewedManuscriptList.
+  */
+  function pop()
+    public
+    returns(uint, bytes32)
+  {
+    uint ManuscriptNum;
+    bytes32 ManuscriptHash;
+    (ManuscriptNum, ManuscriptHash) = PaperQueue.pop();
+    ReviewedList.append(ManuscriptHash);
+    return(ManuscriptNum, ManuscriptHash);
   }
+  /**
+    Function to get all manuscript indexes.
+  */
+  function getAll()
+    constant returns (bytes32[] hashes)
+    {
+      bytes32[] store;
+      var count = ReviewedList.size_list();
+      var it = ReviewedList.iterate_start();
+      while(it != count){
+        store.push(ReviewedList.iterate_get(it));
+        it = ReviewedList.iterate_next(it);
+      }
+      return store;
+    }
 }
