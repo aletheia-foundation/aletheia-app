b470fd0bd1775160ac91122e1df7a62d8d59f1e9
cchrs <cchrs@protonmail.com>
2018-01-03 20:24:23 +0100

    add simple Reputation contract and helper contract SafeMath

--- migrations/0002-deploy-submitted-papers-index.js
+++ migrations/0002-deploy-submitted-papers-index.js
@@ -1,5 +1,7 @@
 var Aletheia = artifacts.require('./Aletheia.sol')
+var Reputation = artifacts.require('../contracts/Reputation.sol')
 
 module.exports = function (deployer) {
-  deployer.deploy(Aletheia)
+  deployer.deploy(Aletheia),
+  deployer.deploy(Reputation)
 }
